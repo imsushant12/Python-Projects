5910b2f1c203c2bf608a3e4245b83ec8fdf73488
imsushant12 <sushantgaurav57@gmail.com>
2021-05-03 23:43:03 +0530

    first commit

---
+++ Pretty Folder/main.py
@@ -0,0 +1,28 @@
+import os
+
+print("\t\t\t\t\t\t\t\t\t ~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("\t\t\t\t\t\t\t\t\t ||  THE PRETTY FOLDER  ||")
+print("\t\t\t\t\t\t\t\t\t ~~~~~~~~~~~~~~~~~~~~~~~~~\n\n")
+
+path = input("Enter the complete path of the folder : ")
+filename = input("Enter that file-name that is not to be altered :  ")
+extension = input("Enter the extension of file which is to be numbered serially : ")
+
+# getting the actual path.
+os.chdir(path)
+# getting all the files and directories in list.
+filesList = os.listdir(path)
+# a variable for numbering the files.
+numbering = 1
+
+for i in filesList:
+    # splitting the files as name and extension.
+    noChange = os.path.splitext(i)
+
+    if(noChange[1] == extension):
+        os.rename(i , f"{numbering}.jpg")
+        numbering += 1
+
+    elif(noChange[0] != filename):
+        new = str(i).capitalize()
+        os.rename(i , new)
