a83f2f900dbf484146416ba6820357de945ac4ae
imsushant12 <sushantgaurav57@gmail.com>
2021-05-21 14:17:51 +0530

    improved code

--- Pretty Folder/main.py
+++ Pretty Folder/main.py
@@ -5,10 +5,10 @@ print("\t\t\t\t\t\t\t\t\t ||  THE PRETTY FOLDER  ||")
 print("\t\t\t\t\t\t\t\t\t ~~~~~~~~~~~~~~~~~~~~~~~~~\n\n")
 
 path = input("Enter the complete path of the folder : ")
-filename = input("Enter that file-name that is not to be altered :  ")
+filename = input("Enter that file-name that you don't want to change :  ")
 extension = input("Enter the extension of file which is to be numbered serially : ")
 
-# getting the actual path.
+# getting the actual path of the folder.
 os.chdir(path)
 # getting all the files and directories in list.
 filesList = os.listdir(path)
@@ -17,6 +17,7 @@ numbering = 1
 
 for i in filesList:
     # splitting the files as name and extension.
+    # noChnage will keep extensions.
     noChange = os.path.splitext(i)
 
     if(noChange[1] == extension):
