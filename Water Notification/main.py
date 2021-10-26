2fe8c7e7aaf755207214155df8a0a3130607d0ac
Sushant Gaurav <sushantgaurav57@gmail.com>
2021-10-26 22:34:32 +0530

    first commit

--- Water Notification/main.py
+++ Water Notification/main.py
@@ -2,11 +2,10 @@ import time
 from plyer import notification
 
 while True:
- 	notification.notify(
- 		title = "**Please Drink Water Now!!",
- 		message ="Drinking Water Helps Maintain the Balance of Body Fluids. So please drink at least 4 liters of water everyday!",
- 		app_icon = "D:\Python\Projects\Water Notification\water.ico",
- 		timeout= 10
-	)
-	# setting reminder for every hour
-	time.sleep(60*60)
+    notification.notify(
+        title="**Please Drink Water Now!!",
+        message="Drinking Water Helps Maintain the Balance of Body Fluids. So please drink at least 4 liters of water everyday!",
+        timeout=10
+
+    )
+    time.sleep(5)
